09bab4d9580d8c5e7c529b86d177003b48b78ae2
SaiVardhanNP <saivardhan.nyalapatla55@gmail.com>
2025-09-04 18:37:54 +0530

    added the websocket for temp chat

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,2 +1,13 @@
-console.log("Initialized node.js Project!");
-export {};
+import { WebSocketServer } from "ws";
+const ws = new WebSocketServer({ port: 5000 });
+ws.on('connection', (socket) => {
+    console.log("Connected");
+    socket.send("Hello from websocket!");
+    socket.on('message', (e) => {
+        if (e.toString() === "PING")
+            socket.send("PONG");
+    });
+    socket.on('disconnect', (message) => {
+        socket.send("Disconnected!");
+    });
+});
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,41 @@
-console.log("Initialized node.js Project!");
+import { WebSocketServer,WebSocket } from "ws";
+
+
+const ws=new WebSocketServer({port:5000});
+
+interface User{
+    socket:WebSocket;
+    room:string;
+}
+
+let allSockets:User[]=[];
+
+ws.on('connection',(socket)=>{
+    socket.on('message',(message)=>{
+        const parsedMessage=JSON.parse(message as unknown as string);
+        if(parsedMessage.type==='join'){
+            allSockets.push({
+                socket,
+                room:parsedMessage.payload.roomId
+            })
+        }
+        if(parsedMessage.type==='chat'){
+            let currentRoom=null;
+            for(let i=0;i<allSockets.length;i++){
+                if(allSockets[i]?.socket==socket){
+                    currentRoom=allSockets[i]?.room;
+                }
+            }
+
+            for(let i=0;i<allSockets.length;i++){
+                if(allSockets[i]?.room==currentRoom){
+                    allSockets[i]?.socket.send(parsedMessage.payload.message);
+                }
+            }
+        }
+
+    })
+    socket.on('close',()=>{
+       allSockets= allSockets.filter((x)=>x.socket!=socket)
+    })
+})
