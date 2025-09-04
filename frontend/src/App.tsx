b7a0cb61cf261b1d35f9c3b4797deb682eebd0dd
SaiVardhanNP <saivardhan.nyalapatla55@gmail.com>
2025-09-04 18:48:43 +0530

    initialized frontend with react

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='h-screen bg-amber-950 flex items-center justify-center'>
+    <h1 className='text-center text-white font-extrabold text-3xl '>Hello WebSockets From React!</h1>
+    </div>
+  )
+}
+
+export default App
