cd4bafe50d8518acc3d0537a0277d094e4cfb1ef
SaiVardhanNP <saivardhan.nyalapatla55@gmail.com>
2025-09-04 18:55:37 +0530

    added username also

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,41 +1,47 @@
-import { WebSocketServer,WebSocket } from "ws";
+import { WebSocketServer, WebSocket } from "ws";
 
+const ws = new WebSocketServer({ port: 5000 });
 
-const ws=new WebSocketServer({port:5000});
-
-interface User{
-    socket:WebSocket;
-    room:string;
+interface User {
+  socket: WebSocket;
+  room: string;
+  name: string;
 }
 
-let allSockets:User[]=[];
+let allSockets: User[] = [];
 
-ws.on('connection',(socket)=>{
-    socket.on('message',(message)=>{
-        const parsedMessage=JSON.parse(message as unknown as string);
-        if(parsedMessage.type==='join'){
-            allSockets.push({
-                socket,
-                room:parsedMessage.payload.roomId
-            })
+ws.on("connection", (socket) => {
+  socket.on("message", (message) => {
+    const parsedMessage = JSON.parse(message as unknown as string);
+    if (parsedMessage.type === "join") {
+      allSockets.push({
+        socket,
+        room: parsedMessage.payload.roomId,
+        name: parsedMessage.payload.name,
+      });
+    }
+    if (parsedMessage.type === "chat") {
+      let currentRoom = null;
+      let senderName = null;
+
+      for (let i = 0; i < allSockets.length; i++) {
+        if (allSockets[i]?.socket == socket) {
+          currentRoom = allSockets[i]?.room;
+          senderName = allSockets[i]?.name;
         }
-        if(parsedMessage.type==='chat'){
-            let currentRoom=null;
-            for(let i=0;i<allSockets.length;i++){
-                if(allSockets[i]?.socket==socket){
-                    currentRoom=allSockets[i]?.room;
-                }
-            }
+      }
 
-            for(let i=0;i<allSockets.length;i++){
-                if(allSockets[i]?.room==currentRoom){
-                    allSockets[i]?.socket.send(parsedMessage.payload.message);
-                }
-            }
+      for (let i = 0; i < allSockets.length; i++) {
+        if (allSockets[i]?.room == currentRoom) {
+            allSockets[i]?.socket.send(JSON.stringify({
+                message: parsedMessage.payload.message,
+                sender: senderName
+            }));
         }
-
-    })
-    socket.on('close',()=>{
-       allSockets= allSockets.filter((x)=>x.socket!=socket)
-    })
-})
+      }
+    }
+  });
+  socket.on("close", () => {
+    allSockets = allSockets.filter((x) => x.socket != socket);
+  });
+});
