05f00fcdcf5de5112b42123b70eb2afb50e0aac9
SaiVardhanNP <saivardhan.nyalapatla55@gmail.com>
2025-09-04 22:44:14 +0530

    removed import react

--- frontend/src/components/ChatRoom.tsx
+++ frontend/src/components/ChatRoom.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from "react";
+import { useEffect, useState, useRef } from "react";
 import { useLocation, useParams } from "react-router-dom";
 
 interface Message {
